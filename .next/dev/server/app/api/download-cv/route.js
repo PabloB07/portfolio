a784777cd3839b1bcdca9e649ed9f6a36611a976
PabloB07 <pabloblanco0798@gmail.com>
2026-02-09 13:02:45 -0300

    deleted .next

--- .next/dev/server/app/api/download-cv/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/download-cv/route.js")
-R.c("server/chunks/node_modules_next_6628cc9c._.js")
-R.c("server/chunks/[root-of-the-server]__af34f2d0._.js")
-R.c("server/chunks/_next-internal_server_app_api_download-cv_route_actions_a828f385.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/download-cv/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/download-cv/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lj8NZowQFTgi3U4mUsB9icYIMOMXr8B8xvUXC25LmmM=\"\n}"
